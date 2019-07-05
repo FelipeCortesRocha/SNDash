b8927dcd62c346c27653a6d1cd4d1b9539abd27c
Felipe Cortes <felipe.rocha@corebiz.com.br>
2019-07-05 15:26:04 -0300

    Adicionando verificação de negação de palavras e atualizando handlebars

--- app/controllers/Helpers.js
+++ app/controllers/Helpers.js
@@ -1,36 +1,36 @@
-const n4j = require('./Neo4jController')
+const n4j = require('./Neo4jController');
 
-const stopWordsPT = "a, agora, ainda, alguem, algum, alguma, algumas, alguns, ampla, amplas, amplo, amplos, ante, antes, ao, aos, após, aquela, aquelas, aquele, aqueles, aquilo, as, ate, atraves, cada, coisa, coisas, com, como, contra, contudo, da, daquele, daqueles, das, de, dela, delas, dele, deles, depois, dessa, dessas, desse, desses, desta, destas, deste, deste, destes, deve, devem, devendo, dever, deverá, deverão, deveria, deveriam, devia, deviam, disse, disso, disto, dito, diz, dizem, do, dos, e, ela, elas, ele, eles, em, enquanto, entre, era, essa, essas, esse, esses, esta, está, estamos, estão, estas, estava, estavam, estávamos, este, estes, estou, eu, fazendo, fazer, feita, feitas, feito, feitos, foi, for, foram, fosse, fossem, grande, grandes, há, isso, isto, já, la, lá, lhe, lhes, lo, mas, me, mesma, mesmas, mesmo, mesmos, meu, meus, minha, minhas, muita, muitas, muito, muitos, na, nas, nem, nessa, nessas, nesta, nestas, ninguem, no, nos, nós, nossa, nossas, nosso, nossos, num, numa, nunca, o, os, ou, outra, outras, outro, outros, para, pela, pelas, pelo, pelos, pequena, pequenas, pequeno, pequenos, per, perante, pode, pude, podendo, poder, poderia, poderiam, podia, podiam, pois, por, porem, porque, posso, pouca, poucas, pouco, poucos, primeiro, primeiros, própria, próprias, próprio, próprios, quais, qual, quando, quanto, quantos, que, quem, são, se, seja, sejam, sem, sempre, sendo, será, serão, seu, seus, si, sido, só, sob, sobre, sua, suas, talvez, tambem, tampouco, te, tem, tendo, tenha, ter, teu, teus, ti, tido, tinha, tinham, toda, todas, todavia, todo, todos, tu, tua, tuas, tudo, última, últimas, último, últimos, um, uma, umas, uns, vendo, ver, vez, vindo, vir, vos, vós"
-const stopWordsEN = "able, about, across, after, all, almost, also, am, among, an, and, any, are, as, at, be, because, been, but, by, can, cannot, could, dear, did, do, does, either, else, ever, every, for, from, get, got, had, has, have, he, her, hers, him, his, how, however, i, if, in, into, is, it, its, just, least, let, may, me, might, most, must, my, neither,of, off, often, on, only, or, other, our, own, rather, said, say, says, she, should, since, so, some, than, that, the, their, them, then, there, these, they, this, tis, to, too, twas, us, wants, was, we, were, what, when, where, which, while, who, whom, why, will, with, would, yet, you, your"
-const negationWords = [' nao ', ' n ', ' nop ', ' not ', ' nada ']
-let positiveWords = []
-let negativeWords = []
-let str
+const stopWordsPT = "a, agora, ainda, alguem, algum, alguma, algumas, alguns, ampla, amplas, amplo, amplos, ante, antes, ao, aos, após, aquela, aquelas, aquele, aqueles, aquilo, as, ate, atraves, cada, coisa, coisas, com, como, contra, contudo, da, daquele, daqueles, das, de, dela, delas, dele, deles, depois, dessa, dessas, desse, desses, desta, destas, deste, deste, destes, deve, devem, devendo, dever, deverá, deverão, deveria, deveriam, devia, deviam, disse, disso, disto, dito, diz, dizem, do, dos, e, ela, elas, ele, eles, em, enquanto, entre, era, essa, essas, esse, esses, esta, está, estamos, estão, estas, estava, estavam, estávamos, este, estes, estou, eu, fazendo, fazer, feita, feitas, feito, feitos, foi, for, foram, fosse, fossem, grande, grandes, há, isso, isto, já, la, lá, lhe, lhes, lo, mas, me, mesma, mesmas, mesmo, mesmos, meu, meus, minha, minhas, muita, muitas, muito, muitos, na, nas, nem, nessa, nessas, nesta, nestas, ninguem, no, nos, nós, nossa, nossas, nosso, nossos, num, numa, nunca, o, os, ou, outra, outras, outro, outros, para, pela, pelas, pelo, pelos, pequena, pequenas, pequeno, pequenos, per, perante, pode, pude, podendo, poder, poderia, poderiam, podia, podiam, pois, por, porem, porque, posso, pouca, poucas, pouco, poucos, primeiro, primeiros, própria, próprias, próprio, próprios, quais, qual, quando, quanto, quantos, que, quem, são, se, seja, sejam, sem, sempre, sendo, será, serão, seu, seus, si, sido, só, sob, sobre, sua, suas, talvez, tambem, tampouco, te, tem, tendo, tenha, ter, teu, teus, ti, tido, tinha, tinham, toda, todas, todavia, todo, todos, tu, tua, tuas, tudo, última, últimas, último, últimos, um, uma, umas, uns, vendo, ver, vez, vindo, vir, vos, vós";
+const stopWordsEN = "able, about, across, after, all, almost, also, am, among, an, and, any, are, as, at, be, because, been, but, by, can, cannot, could, dear, did, do, does, either, else, ever, every, for, from, get, got, had, has, have, he, her, hers, him, his, how, however, i, if, in, into, is, it, its, just, least, let, may, me, might, most, must, my, neither,of, off, often, on, only, or, other, our, own, rather, said, say, says, she, should, since, so, some, than, that, the, their, them, then, there, these, they, this, tis, to, too, twas, us, wants, was, we, were, what, when, where, which, while, who, whom, why, will, with, would, yet, you, your";
+const negationWords = [' nao ', ' n ', ' nop ', ' not ', ' nada ', ' non '];
+let positiveWords = [];
+let negativeWords = [];
+let str;
 
-const convertDate = (inputFormat) => {
+const convertDate = inputFormat => {
     const pad = (s) => (s < 10) ? '0' + s : s;
     let d = new Date(inputFormat);
     return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join('/');
-}
+};
 
-const formatTweet = (tweet) => {
-    let date = new Date(tweet.created_at)
+const formatTweet = tweet => {
+    let date = new Date(tweet.created_at);
     return {
         created_at: `${convertDate(date)} - ${date.toLocaleTimeString('pt-BR')}`,
         text: tweet.full_text,
         url: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
-    }
-}
+    };
+};
 
 const getUnique = (arr, comp) => {
     return unique = arr
         .map(e => e[comp])
         .map((e, i, final) => final.indexOf(e) === i && i)
-        .filter(e => arr[e]).map(e => arr[e])
-}
+        .filter(e => arr[e]).map(e => arr[e]);
+};
 
 const removerAcentos = newStringComAcento => {
-    let string = newStringComAcento
+    let string = newStringComAcento;
     let mapaAcentosHex = {
         a: /[\xE0-\xE6]/g,
         e: /[\xE8-\xEB]/g,
@@ -39,25 +39,25 @@ const removerAcentos = newStringComAcento => {
         u: /[\xF9-\xFC]/g,
         c: /\xE7/g,
         n: /\xF1/g
-    }
+    };
 
     for (let letra in mapaAcentosHex) {
-        let expressaoRegular = mapaAcentosHex[letra]
-        string = string.replace(expressaoRegular, letra)
+        let expressaoRegular = mapaAcentosHex[letra];
+        string = string.replace(expressaoRegular, letra);
     }
 
-    return string
-}
+    return string;
+};
 
 const removeStopWords = str => {
-    let x
-    let y
-    let word
-    let stop_word
-    let regex_str
-    let regex
-    let cleansed_string = str
-    let stop_words = [...stopWordsPT.split(',')].concat([...stopWordsEN.split(',')])
+    let x,
+    y,
+    word,
+    stop_word,
+    regex_str,
+    regex,
+    cleansed_string = str,
+    stop_words = [...stopWordsPT.split(',')].concat([...stopWordsEN.split(',')])
     stop_words = stop_words.map(word => word.trim())
 
     // Split out all the individual words in the phrase
@@ -88,27 +88,34 @@ const removeStopWords = str => {
         }
     }
     return cleansed_string.replace(/^\s+|\s+$/g, "");
-}
+};
 
 const checkWord = word => {
-    if (Array.isArray(word.portuguese)) {
-        word.portuguese.forEach(w => {
-            if (str.indexOf(w) != -1) return word;
-        })
-    }
-    if (Array.isArray(word.english)) {
-        word.english.forEach(w => {
-            if (str.indexOf(w) != -1) return word;
-        })
-    }
-    if (Array.isArray(word.spanish)) {
-        word.spanish.forEach(w => {
-            if (str.indexOf(w) != -1) return word;
-        })
-    }
-    if (str.indexOf(word.portuguese) != -1 || str.indexOf(word.english) != -1 || str.indexOf(word.spanish) != -1) return word;
+    let haveWord = false;
+    let wordWithNegation = false;
+
+    if (!Array.isArray(word.portuguese)) word.portuguese = [word.portuguese];
+    if (!Array.isArray(word.english)) word.english = [word.english];
+    if (!Array.isArray(word.spanish)) word.spanish = [word.spanish];
+
+    const check = w => {
+        const indexOf = str.indexOf(w);
+        if (indexOf != -1) {
+            haveWord = w;
+            let subStr = str.substr(indexOf-6, 6);
+            for (const negWord of negationWords) {
+                if(subStr.indexOf(negWord) !== -1) wordWithNegation = true;
+            };
+        }
+    };
+
+    word.portuguese.forEach(check);
+    word.english.forEach(check);
+    word.spanish.forEach(check);
+    
+    if (haveWord && !wordWithNegation) return haveWord
     return false;
-}
+};
 
 const checkSentiment = tweetString => {
 
@@ -116,23 +123,17 @@ const checkSentiment = tweetString => {
     str = removerAcentos(str);
     str = removeStopWords(str);
 
-    let positives = positiveWords.filter(word => {
-        return checkWord(word);
-    });
-    let negatives = negativeWords.filter(word => {
-        return checkWord(word);
-    });
+    let positives = positiveWords.filter(word => checkWord(word));
+    let negatives = negativeWords.filter(word => checkWord(word));
 
     let totalWordsFound = positives.length + negatives.length;
-
-    if (totalWordsFound > 0) console.log(str);
 
     return {
         totalWordsFound,
         positiveRate: ((positives.length / totalWordsFound) * 100) || 0,
         negativeRate: ((negatives.length / totalWordsFound) * 100) || 0
     };
-}
+};
 
 const checkTweets = tweets => {
     return new Promise(async(resolve) => {
@@ -146,7 +147,7 @@ const checkTweets = tweets => {
             if (aDate < bDate) return -1;
             if (aDate > bDate) return 1;
             return 0;
-        })
+        });
 
         const addTimeline = (analise, tweet, type) => {
             let data = new Date(tweet.created_at);
@@ -157,7 +158,7 @@ const checkTweets = tweets => {
                 analise.timeline.negatives[analise.timeline.labels.length - 1] = 0;
             }
             analise.timeline[type][analise.timeline.labels.length - 1]++;
-        }
+        };
 
         const checkLocation = (analise, tweet, type) => {
             if (tweet.geo) {
@@ -189,7 +190,7 @@ const checkTweets = tweets => {
                 }
             }
             return false;
-        }
+        };
 
         // Reduce para gerar o objeto da análise
         let result = tweets.reduce((analise, tweet) => {
@@ -249,7 +250,7 @@ const checkTweets = tweets => {
             negativeTweets: 0
         });
         resolve(result);
-    })
+    });
 }
 
 module.exports.getUnique = getUnique;
